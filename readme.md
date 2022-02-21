868afb8ed0f3391a4b285ad45ae251f6b9dde940
Elias Prescott <85904049+EliasPrescott@users.noreply.github.com>
2022-02-21 17:39:01 -0600

    Adding readme, adding recursive parsing, and switching base parser functions to use chars properly

---
+++ readme.md
@@ -0,0 +1,39 @@
+# Rust Language Compiler
+
+### About
+I am trying to build a complete language parser and compiler using Rust. The goal is to not use any external crates or libraries, and to eventually target LLVM or some other well-respected compilation target. I'm not sure what the language's syntax will look like yet, but it will likely be some mix of Rust and F#, as I like the idea of all control structures being expressions.
+
+### Parsing (In-Progress)
+I am currently building out the parsing system. It is a hand-made recursive descent parser. I'm trying to keep the parser generic and seperate enough that it could be used for other projects. It can currently only parse brace-defined scopes, integer assignments, and variable references. Most of the basic parsing methods are in-place though, so it should be easier now to add more advanced parsing rules.
+
+#### Current Parsing Example:
+Here is some pseudo-code input that the parser can currently handle:
+```
+let x = 0;
+let y = 100;
+
+{
+    {
+        let testing_var = 23253524;
+        testing_var
+    }
+}
+```
+
+Here is the corresponding output:
+```Rust
+[
+    ASTAssignment("x", ASTInteger(0)),
+    ASTAssignment("y", ASTInteger(100)),
+    ASTScope(
+        [
+            ASTScope(
+                [
+                    ASTAssignment("testing_var", ASTInteger(23253524)),
+                    ASTVariableRef("testing_var"),
+                ],
+            ),
+        ],
+    ),
+]
+```
--- src/parsing.rs
+++ src/parsing.rs
@@ -6,10 +6,12 @@ pub struct ParseInput {
 #[derive(Clone, Copy)]
 pub struct ParseSavePoint(usize);
 
+pub type CharIndice = (usize, char);
+
 impl ParseInput {
     /// Gets an option of the next character without moving the cursor
-    pub fn get_next_char(&self) -> Option<char> {
-        self.text.chars().into_iter().nth(self.position)
+    pub fn get_next_char(&self) -> Option<CharIndice> {
+        self.text.char_indices().into_iter().nth(self.position)
     }
 
     pub fn create_save_point(&self) -> ParseSavePoint {
@@ -21,15 +23,15 @@ impl ParseInput {
     }
 
     pub fn get_next_char_result(&self) -> Result<char, String> {
-        match self.text.chars().into_iter().nth(self.position) {
-            Some(char) => Ok(char),
+        match self.get_next_char() {
+            Some((_, char)) => Ok(char),
             None => Err(String::from("Expected character, but found end of parser input"))
         }
     }
 
     pub fn get_next_char_predicate(&self, predicate: &dyn Fn(char) -> Result<char, String>) -> Result<char, String> {
-        match self.text.chars().into_iter().nth(self.position) {
-            Some(char) => predicate(char),
+        match self.get_next_char() {
+            Some((_, char)) => predicate(char),
             None => Err(String::from("Expected character, but found end of parser input"))
         }
     }
@@ -54,20 +56,22 @@ impl ParseInput {
         )
     }
 
+    /// Uses char indices now
     /// Gets an option of the next character and advances the cursor position by one
     pub fn pop_next_char(&mut self) -> Option<char> {
-        match self.get_next_char() {
-            Some(char) => {
-                self.position += 1;
-                Some(char)
-            },
-            None => None
-        }        
+        if let Some((_, char)) = self.text.char_indices().into_iter().nth(self.position) {
+            if let Some((index, _)) = self.text.char_indices().into_iter().nth(self.position + 1) {
+                self.position = index;
+            }
+            Some(char)
+        } else {
+            None
+        }
     }
 
     pub fn pop_next_char_result(&mut self) -> Result<char, String> {
         match self.get_next_char() {
-            Some(char) => {
+            Some((index, char)) => {
                 self.position += 1;
                 Ok(char)
             },
@@ -77,7 +81,7 @@ impl ParseInput {
 
     pub fn pop_next_char_predicate(&mut self, predicate: &dyn Fn(char) -> Result<char, String>) -> Result<char, String> {
         match self.get_next_char() {
-            Some(char) => {
+            Some((index, char)) => {
                 let x = predicate(char);
                 if x.is_ok() {
                     self.position += 1;
@@ -108,29 +112,59 @@ impl ParseInput {
         )
     }
 
-    /// Advances the cursor position by one. Should only be used when the next character is known.
+    /// Uses char indices properly
     pub fn skip_next_char(&mut self) {
-        self.position += 1;
+        if let Some((index, _)) = self.text.char_indices().nth(self.position + 1) {
+            self.position = index;
+        } else {
+            self.position = self.text.char_indices().collect::<Vec<(usize, char)>>().len();
+        }
     }
 
     /// Skips the next x number of characters
     pub fn skip_x_chars(&mut self, x: usize) {
-        self.position += x;       
+        for _ in 0..x {
+            if let Some((index, _)) = self.text.char_indices().nth(self.position + 1) {
+                self.position = index;
+            }
+        }
     }
 
+    /// Uses char indices properly.
     /// Gets the text from the cursor position onwards
-    pub fn get_remaining_text(&self) -> &str {
-        &self.text[self.position..]
+    pub fn get_remaining_text(&self) -> Result<String, String> {
+        match self.text.chars().collect::<Vec<char>>().get(self.position..) {
+            Some(chars) => {
+                let mut output = String::new();
+                for c in chars {
+                    output += &c.to_string();
+                }
+                Ok(output)
+            },
+            None => {
+                Err("Invalid input access".to_string())
+            }
+        }
     }
 
     /// Gets the next x number of characters without moving the cursor
-    pub fn get_next_x_chars(&self, x: usize) -> Option<&str> {
-        self.text.get(self.position..x)
+    pub fn get_next_x_chars(&self, x: usize) -> Option<String> {
+        let mut output = String::new();
+        let mut current_index = self.position;
+        for _ in self.position..x+self.position {
+            if let Some((_, c)) = self.text.char_indices().nth(current_index) {
+                output += &c.to_string();
+                current_index += 1;
+            } else {
+                return None;
+            }
+        }
+        Some(output)
     }
 
     /// Determines if the next block of characters is equal to the predicate string
     pub fn match_word(&self, predicate: &str) -> bool {
-        self.get_next_x_chars(predicate.len()) == Some(predicate)
+        self.get_next_x_chars(predicate.len()) == Some(predicate.to_string())
     }
 
     pub fn finished(&self) -> bool {
@@ -140,7 +174,7 @@ impl ParseInput {
     /// Skips the cursor past an expected character, and returns an error message if the expected character is not found.
     pub fn skip_char(&mut self, predicate: char) -> Result<(), String> {
         match self.get_next_char() {
-            Some(next_char) => {
+            Some((_, next_char)) => {
                 if next_char == predicate {
                     self.skip_next_char();
                     Ok(())
@@ -158,13 +192,12 @@ impl ParseInput {
             self.skip_x_chars(predicate.len());
             Ok(())
         } else {
-
-            Err(format!("Expected: keyword '{}'", predicate))
+            Err(format!("Expected keyword '{}'", predicate))
         }
     }
 
     pub fn skip_any_of_char(&mut self, skip_char: char) {
-        while self.get_next_char() == Some(skip_char) {
+        while let Some((_, skip_char)) = self.get_next_char()  {
             self.skip_next_char();
         }
     }
@@ -172,7 +205,7 @@ impl ParseInput {
     pub fn skip_any_of_chars(&mut self, skip_chars: Vec<char>) {
         loop {
             match self.get_next_char() {
-                Some(next_char) => {
+                Some((index, next_char)) => {
                     if skip_chars.contains(&next_char) {
                         self.skip_next_char()
                     } else {
@@ -194,7 +227,7 @@ impl ParseInput {
 
     pub fn pop_until_char(&mut self, stop_char: char) -> String {
         let mut output = "".to_string();
-        while let Some(next_char) = self.get_next_char() {
+        while let Some((index, next_char)) = self.get_next_char() {
             if next_char == stop_char {
                 return output;
             } else {
@@ -207,7 +240,7 @@ impl ParseInput {
 
     pub fn pop_until_chars(&mut self, stop_chars: Vec<char>) -> String {
         let mut output = "".to_string();
-        while let Some(next_char) = self.get_next_char() {
+        while let Some((index, next_char)) = self.get_next_char() {
             if stop_chars.contains(&next_char) {
                 return output;
             } else {
