19afc08b8af530c7e167ca1721c66bcac80fb7b2
Elias Prescott <85904049+EliasPrescott@users.noreply.github.com>
2022-02-21 17:39:01 -0600

    Adding readme, adding recursive parsing, and switching parser functions to use chars properly

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 mod parsing;
 
-use std::fs;
+use std::{fs, rc::Rc, cell::RefCell};
 use parsing::*;
 
 const TESTING_FILE_PATH: &str = "test.txt";
-
-type ASTNameRaw = (String);
 
 #[derive(Debug)]
 enum ASTExpression {
     ASTVariableRef(String),
     ASTInteger(i64),
+
+    // I'm only parsing integer assignment right now, but this variant is ready for future use by accepting all expression types.
     ASTAssignment(String, Box<ASTExpression>),
-    Unit,
+    ASTScope(Vec<Box<ASTExpression>>),
+    ASTUnit,
 }
 
 fn parse_integer(input: &mut ParseInput) -> Result<i64, String> {
-    let first_char = input.get_next_char_numerical()?;
+    // The pop_next_char_numerical and other similar methods only mutate the ParseInput if the next char matches the predicate.
+    // This fact will need to be explicit in the documentation for these methods.
+    let first_char = input.pop_next_char_numerical()?;
     let mut output = first_char.to_string();
-    input.skip_next_char();
-    while let Ok(next_char) = input.get_next_char_numerical() {
+    while let Ok(next_char) = input.pop_next_char_numerical() {
         output += &next_char.to_string();
-        input.skip_next_char();
     }
     Ok(str::parse::<i64>(&output).map_err(|err| err.to_string())?)
 }
@@ -36,8 +37,33 @@ fn parse_name(input: &mut ParseInput) -> Result<String, String> {
     Ok(output)
 }
 
+// Since this pattern is so permissive, it is important to put it after other, stricter patterns.
+// It could be good to make a list of language keywords (e.g. 'let') and forbid them from being parsed as variable names.
 fn parse_variable_ref(input: &mut ParseInput) -> Result<ASTExpression, String> {
     Ok(ASTExpression::ASTVariableRef(parse_name(input)?))
+}
+
+// Instead of parsing input directly, this function takes in an interior_parser, and then builds a new parser that will continually run that interior_parser within a braced scope.
+fn parse_scope_with_parser(interior_parser: Box<dyn Fn(&mut ParseInput) -> Result<ASTExpression, String>>) -> Box<dyn Fn(&mut ParseInput) -> Result<ASTExpression, String>> {
+    Box::new(move | input: &mut ParseInput | {
+        let mut output = vec!();
+        input.skip_char('{')?;
+        loop {
+            input.skip_spaces_and_newlines();
+            match interior_parser(input) {
+                Ok(x) => {
+                    output.push(Box::new(x));
+                },
+                Err(e) => {
+                    if let Ok(()) = input.skip_char('}') {
+                        return Ok(ASTExpression::ASTScope(output));
+                    } else {
+                        return Err(e);
+                    }
+                }
+            }
+        }
+    })
 }
 
 fn parse_integer_assignment(input: &mut ParseInput) -> Result<ASTExpression, String> {
@@ -53,31 +79,55 @@ fn parse_integer_assignment(input: &mut ParseInput) -> Result<ASTExpression, Str
 }
 
 /// Tries every parser in a list. Returns the first successful parse result, or the last error if all fail. 
-fn try_parsers<T>(input: &mut ParseInput, parsers: Vec<&dyn Fn(&mut ParseInput) -> Result<T, String>>) -> Result<T, String> {
-    let save_point = input.create_save_point();
-    let mut last_err = String::new();
-    for parser in parsers {
-        match parser(input) {
-            Ok(x) => return Ok(x),
-            Err(err) => {
-                last_err = err;
-                input.load_save_point(save_point);
+fn try_parsers_with_list<'a>(parsers: Rc<RefCell<Vec<Box<dyn for<'r> Fn(&'r mut ParseInput) -> Result<ASTExpression, String>>>>>) -> Box<dyn for<'r> Fn(&'r mut ParseInput) -> Result<ASTExpression, String> + 'a> {
+    Box::new(move | input: &mut ParseInput | -> Result<ASTExpression, String> {
+        let save_point = input.create_save_point();
+        let mut last_err = String::new();
+        for parser in parsers.borrow().iter() {
+            match parser(input) {
+                Ok(x) => return Ok(x),
+                Err(err) => {
+                    last_err = err;
+                    input.load_save_point(save_point);
+                }
             }
         }
-    }
-    Err(last_err.to_string())
+        Err(last_err.to_string())
+    })
 }
 
 fn main() {
     match fs::read_to_string(TESTING_FILE_PATH) {
         Ok(contents) => {
+            let parsers: Rc<RefCell<Vec<Box<dyn for<'r> Fn(&'r mut ParseInput) -> Result<ASTExpression, String>>>>> = Rc::new(RefCell::new(vec!(
+                Box::new(parse_integer_assignment), 
+                Box::new(parse_variable_ref),
+            )));
+
+            // I don't think I should need to make this parser twice, but I'm how it could be shared properly.
+            // Building it a few times likely adds very little overhead, so it's probably not a huge concern.
+            let main_parser = try_parsers_with_list(parsers.clone());
+            let main_parser_ref = try_parsers_with_list(parsers.clone());
+
+            let scope_parser = parse_scope_with_parser(Box::new(main_parser_ref));
+
+            // After constructing the scope_parser and passing the main parser into it, I then add the scope_parser into the main parser.
+            // This allows for endless recursive parsing, but it also makes the type definitions explode in length.
+            // A Rc<RefCell<Vec<Box<...>>>> doesn't exactly roll off the tongue.
+            parsers.borrow_mut().push(Box::new(scope_parser));
+
             let mut input = ParseInput::new(contents);
+
+            let mut ast_tree: Vec<ASTExpression> = Vec::new();
+
+            // The below loop should probably go into a parse_file function somewhere.
+            // If I make a Parser type for my parsing functions, maybe it could live in that type as a static method.
             loop {
                 if !input.finished() {
                     input.skip_spaces_and_newlines();
-                    match try_parsers(&mut input, vec!(&parse_integer_assignment, &parse_variable_ref)) {
+                    match main_parser(&mut input) {
                         Ok(expr) => {
-                            println!("{:?}", expr);
+                            ast_tree.push(expr);
                         },
                         Err(err) => {
                             eprintln!("{}", err);
@@ -87,8 +137,11 @@ fn main() {
                 } else {
                     break;
                 }
-            }            
+            }
+
+            // Only just learned this, but :#? allows for pretty-printing the AST output.
+            println!("{:#?}", ast_tree);
         },
-        Err(err) => println!("{}", err)
+        Err(err) => println!("{}", err)        
     }
 }
